20bf2b1916943af93f003f5b30071f76dbecb248
Benriez <stberiez@stud.h-da.de>
2020-06-30 16:30:06 +0200

    added v-if and v-else

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ new Vue({
     },
     methods: {
         formSubmitted: function(){
-            this.guestName.push(this.newNameText)
-            this.newNameText= ''
+            if(this.newNameText.length > 0){
+                this.guestName.push(this.newNameText)
+                this.newNameText= ''
+            }else{
+                alert('wrong name')
+            }
         }
     },
 });
